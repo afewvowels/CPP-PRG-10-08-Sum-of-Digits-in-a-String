d5ea7c7d20151b8e4da931d86089962eb31bd035
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-02 13:26:05 -0400

    Prompt written.

--- CPP-PRG-10-08-Sum-of-Digits-in-a-String/main.cpp
+++ CPP-PRG-10-08-Sum-of-Digits-in-a-String/main.cpp
@@ -5,11 +5,19 @@
 //  Created by Keith Smith on 11/2/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that asks the user to enter a series of single-digit numbers with nothing
+//  separating them. Read the input as a C-string or a string object. The program
+//  should display the sum of all the single-digit numbers in the string. For example, if the
+//  user enters 2514, the program should display 12, which is the sum of 2, 5, 1, and 4.
+//  The program should also display the highest and lowest digits in the string.
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    
     return 0;
 }
