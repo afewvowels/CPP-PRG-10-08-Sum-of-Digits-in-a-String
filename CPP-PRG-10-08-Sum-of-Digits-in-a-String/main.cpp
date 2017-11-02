9c2abc2755731d6eb024c42b8654f8784d16d23a
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-02 13:22:55 -0400

    Initial Commit

---
+++ CPP-PRG-10-08-Sum-of-Digits-in-a-String/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP-PRG-10-08-Sum-of-Digits-in-a-String
+//
+//  Created by Keith Smith on 11/2/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
