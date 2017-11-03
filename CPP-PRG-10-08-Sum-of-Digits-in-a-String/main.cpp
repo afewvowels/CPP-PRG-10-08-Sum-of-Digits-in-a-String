d8a0906756d7e9cd05f53935bd69acf9b6bdd156
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-02 21:27:03 -0400

    Tested and working.

--- CPP-PRG-10-08-Sum-of-Digits-in-a-String/main.cpp
+++ CPP-PRG-10-08-Sum-of-Digits-in-a-String/main.cpp
@@ -13,11 +13,109 @@
 //
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+const int INT_ARR_SIZE = 99;
+
+char *getCharArray();
+void sumNumbers(char *, int *, int *, int *);
+void displayResults(char *, int *, int *, int *);
+
 int main()
 {
+    char *chrArray = nullptr;
+    
+    int *intHigh = nullptr;
+    intHigh = new int;
+    *intHigh = INT_MIN;
+    
+    int *intLow = nullptr;
+    intLow = new int;
+    *intLow = INT_MAX;
+    
+    int *intSumNumbers = nullptr;
+    intSumNumbers = new int;
+    
+    chrArray = getCharArray();
+    
+    sumNumbers(chrArray, intHigh, intLow, intSumNumbers);
+    
+    displayResults(chrArray, intSumNumbers, intHigh, intLow);
     
     return 0;
+}
+
+char *getCharArray()
+{
+    int *intArrSize = nullptr;
+    intArrSize = new int;
+    *intArrSize = INT_ARR_SIZE;
+    
+    char *chrArr = nullptr;
+    chrArr = new char[*intArrSize];
+    
+    cout << "Please enter a series of up to 99 numbers:\n";
+    cin.getline(chrArr, *intArrSize);
+    
+    return chrArr;
+}
+
+void sumNumbers(char *chrArr, int *intHigh, int *intLow, int *intSum)
+{
+    int *intArrSize = nullptr;
+    intArrSize = new int;
+    *intArrSize = INT_ARR_SIZE;
+    
+    int *intTemp = nullptr;
+    intTemp = new int;
+    
+//    int *intSum = nullptr;
+//    intSum = new int;
+//    *intSum = 0;
+    
+    char *chrTemp = nullptr;
+    chrTemp = new char;
+    
+    for (int i = 0 ; i < *intArrSize ; i++)
+    {
+        *chrTemp = chrArr[i];
+        
+        if (*chrTemp == '\0')
+        {
+            break;
+        }
+        
+        *intTemp = atoi(chrTemp);
+        
+        *intSum += *intTemp;
+        
+        if (*intTemp < *intLow)
+        {
+            *intLow = *intTemp;
+        }
+        
+        if (*intTemp > *intHigh)
+        {
+            *intHigh = *intTemp;
+        }
+    }
+}
+
+void displayResults(char *chrArr, int *intSum, int *intHigh, int *intLow)
+{
+    int i = 0;
+    
+    cout << "The sum of ";
+    
+    while(chrArr[i] != '\0')
+    {
+        cout << chrArr[i];
+        i++;
+    }
+    
+    cout << " is " << *intSum << endl;
+    cout << "The lowest number was: " << *intLow << endl;
+    cout << "The highest number was: " << *intHigh << endl;
 }
